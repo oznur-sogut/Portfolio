924d32e3afd11bd6516880993075815b5e4bfbf3
Öznur <oznur.sgt27@gmail.com>
2024-10-03 10:14:52 +0300

    master

--- Portfolio/Controllers/ExperienceController.cs
+++ Portfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DataAccessLayer.Context;
+using Portfolio.DataAccessLayer.Entities;
 
 namespace Portfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        PortfolioContext context = new PortfolioContext();
         public IActionResult ExperienceList()
         {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = context.Experiences.Find(id);
+            context.Experiences.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience (int id)
+        {
+            var values=context.Experiences.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience (Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
