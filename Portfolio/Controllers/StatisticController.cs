0979e02e39a7bb99d047974b45bd862366cf2819
Öznur <oznur.sgt27@gmail.com>
2024-10-03 20:21:08 +0300

    master

---
+++ Portfolio/Controllers/StatisticController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DataAccessLayer.Context;
+
+namespace Portfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IActionResult Index()
+        {
+
+            ViewBag.skill = context.Skillss.Count();
+            ViewBag.message = context.Messages.Count();
+            ViewBag.messageRead = context.Messages.Where(x => x.IsRead.Equals(true)).Count();
+            ViewBag.messageNotRead = context.Messages.Where(x => x.IsRead.Equals(false)).Count();
+            ViewBag.reference = context.Testimonials.Count();
+            ViewBag.experience = context.Experiences.Count();
+            ViewBag.project = context.MyPortfolios.Count();
+            return View();
+        }
+    }
+}
