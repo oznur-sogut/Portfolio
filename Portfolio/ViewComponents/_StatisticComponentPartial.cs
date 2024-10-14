a48e158167c8d161edebfa339a1e9c64aaf1fe1e
Öznur <oznur.sgt27@gmail.com>
2024-10-14 10:02:35 +0300

    master

--- Portfolio/ViewComponents/_StatisticComponentPartial.cs
+++ Portfolio/ViewComponents/_StatisticComponentPartial.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DataAccessLayer.Context;
 
 namespace Portfolio.ViewComponents
 {
     public class _StatisticComponentPartial: ViewComponent
     {
+        PortfolioContext context= new PortfolioContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.testimonial=context.Testimonials.Select(x => x.TestimonialID).Count();
+            ViewBag.skill=context.Skillss.Select(x => x.SkillsID).Count();
+            ViewBag.portfolio=context.MyPortfolios.Select(x => x.MyPortfolioID).Count();
+            ViewBag.experience=context.Experiences.Select(x => x.ExperienceID).Count();
             return View();
         }
     }
