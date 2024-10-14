a6a0c5d3736be53cc5982d1050575cc0d87a4fc5
Öznur <oznur.sgt27@gmail.com>
2024-10-14 15:51:50 +0300

    master

---
+++ Portfolio/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DataAccessLayer.Context;
+using Portfolio.DataAccessLayer.Entities;
+
+namespace Portfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        PortfolioContext context=new PortfolioContext();
+        public IActionResult AboutList()
+        {
+            var value=context.Abouts.ToList();
+            return View(value);
+        }
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = context.Abouts.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateAbout(About about)
+        {
+            context.Abouts.Update(about);
+            context.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+    }
+}
--- Portfolio/Controllers/MyPortfolioController.cs
+++ Portfolio/Controllers/MyPortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolio.DataAccessLayer.Context;
+using Portfolio.DataAccessLayer.Entities;
 
 namespace Portfolio.Controllers
 {
@@ -11,5 +12,37 @@ namespace Portfolio.Controllers
            var value= context.MyPortfolios.ToList();
             return View(value);
         }
+        [HttpGet]
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreatePortfolio (MyPortfolio myPortfolio)
+        {
+            context.MyPortfolios.Add(myPortfolio);
+            context.SaveChanges();
+            return RedirectToAction("MyPortfolioList");
+        }
+        [HttpGet]
+        public IActionResult UpdatePortfolio (int id)
+        {
+            var value= context.MyPortfolios.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePortfolio (MyPortfolio myPortfolio)
+        {
+            context.MyPortfolios.Update(myPortfolio);
+            context.SaveChanges();
+            return RedirectToAction("MyPortfolioList");
+        }
+        public IActionResult OpenPortfolio(int id)
+        {
+            var value = context.MyPortfolios.Find(id);
+            return View(value);
+        }
+
     }
 }
